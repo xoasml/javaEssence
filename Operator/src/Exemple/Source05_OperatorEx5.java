257f9b4c34cbb0ab89cd6ef4de81b43f4584fd73
TaeHoonKim <xoasml@gmail.com>
2020-12-01 23:40:07 +0900

    test3

--- Operator/src/Exemple/Source05_OperatorEx5.java
+++ Operator/src/Exemple/Source05_OperatorEx5.java
@@ -4,5 +4,14 @@ public class Source05_OperatorEx5 {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 4;
+		
+		System.out.printf("111");
+		System.out.printf("");
+		System.out.printf("");
+		System.out.printf("");
+		System.out.printf("");
+		System.out.printf("");
+		
+		
 	}
 }
