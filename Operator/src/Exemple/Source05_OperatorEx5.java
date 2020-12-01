a7b97185ff5e352ac5431b6601ff9fb9f9676817
TaeHoonKim <xoasml@gmail.com>
2020-12-02 00:24:39 +0900

    2020.12.02 first commit Operator(연산자)

--- Operator/src/Exemple/Source05_OperatorEx5.java
+++ Operator/src/Exemple/Source05_OperatorEx5.java
@@ -2,16 +2,18 @@ package Exemple;
 
 public class Source05_OperatorEx5 {
 	public static void main(String[] args) {
+		
 		int a = 10;
 		int b = 4;
 		
-		System.out.printf("111");
-		System.out.printf("");
-		System.out.printf("");
-		System.out.printf("");
-		System.out.printf("");
-		System.out.printf("");
+		System.out.printf("%d + %d = %d%n", a, b, a + b);
+		System.out.printf("%d - %d = %d%n", a, b, a - b);
+		System.out.printf("%d * %d = %d%n", a, b, a * b);
+		System.out.printf("%d / %d = %d%n", a, b, a / b);
+		System.out.printf("%d / %f = %f%n", a, (float)b, a / (float)b);
 		
+		float e = 0.01f / 0;
+		double h = 0.01 / 0;
 		
 	}
 }
