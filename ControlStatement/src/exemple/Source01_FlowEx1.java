481a9750a3761ad0f02f438309a84888a0c3f31d
TaeHoonKim <xoasml@gmail.com>
2020-12-04 14:36:38 +0900

    2020.12.04 Flow - if Commit

--- ControlStatement/src/exemple/Source01_FlowEx1.java
+++ ControlStatement/src/exemple/Source01_FlowEx1.java
@@ -4,5 +4,16 @@ public class Source01_FlowEx1 {
 	public static void main(String[] args) {
 		int x = 0;
 		System.out.printf("x=%d 일 때, 참인 것은%n", x);
+		
+		if(x==0) System.out.println("x==0");
+		if(x!=0) System.out.println("x!0");
+		if(!(x==0))	System.out.println("!(x==0)");
+		if(!(x!=0))	System.out.println("!(x!=0)");
+		
+		x = 1;
+		if(x==0) System.out.println("x==0");
+		if(x!=0) System.out.println("x!0");
+		if(!(x==0))	System.out.println("!(x==0)");
+		if(!(x!=0))	System.out.println("!(x!=0)");
 	}
 }
