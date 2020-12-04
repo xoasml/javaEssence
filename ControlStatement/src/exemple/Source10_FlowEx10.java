f20f3d87acb962ce4e04cc48d53062696af606d9
TaeHoonKim <xoasml@gmail.com>
2020-12-04 17:00:03 +0900

    2020.12.04 switch

---
+++ ControlStatement/src/exemple/Source10_FlowEx10.java
@@ -0,0 +1,32 @@
+package exemple;
+
+import java.util.Scanner;
+
+public class Source10_FlowEx10 {
+	public static void main(String[] args) {
+		
+		int score = 0;
+		char grade = ' ';
+		
+		System.out.print("당신의 점수를 입력하세요. (1~100)>  ");
+		Scanner scan = new Scanner(System.in);
+		score = scan.nextInt();
+		
+		switch (score / 10) {
+			case 10 : case 9 :
+				grade = 'A';
+				break;
+			case 8 :
+				grade = 'B';
+				break;
+			case 7 :
+				grade = 'C';
+				break;
+			default :
+				grade = 'F';
+		}
+		
+		System.out.println(String.format("당신의 점수는 %d, 등급은 %c 입니다.", score, grade));
+		
+	}
+}
---
+++ ControlStatement/src/exemple/Source11_FlowEx11.java
@@ -0,0 +1,36 @@
+package exemple;
+
+import java.util.Scanner;
+
+public class Source11_FlowEx11 {
+	public static void main(String[] args) {
+		System.out.println("당신의 주민등록번호를 입력 하세요 > ");
+		Scanner scan = new Scanner(System.in);
+		
+		String regNo = scan.nextLine();
+		char gender = regNo.charAt(7);
+		
+		switch (gender) {
+			case '1': case '3':
+				switch(gender) {
+					case '1':
+						System.out.println("당신은 2000년 이전 출생한 남자");
+						break;
+					case '3':
+						System.out.println("당신은 2000년 이후 출생한 남자");
+				}
+				break;
+			case '2': case '4':
+				switch(gender) {
+					case '2':
+						System.out.println("당신은 2000년 이전 출생한 여자");
+					break;
+					case '4':
+						System.out.println("당신은 2000년 이후 출생한 여자");
+				}
+				break;
+			default:
+				break;
+		}
+	}
+}
